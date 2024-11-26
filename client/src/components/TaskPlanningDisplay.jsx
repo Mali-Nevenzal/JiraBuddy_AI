2c25e12c6560301da54f460650654151e8b03276
= <=>
2024-11-26 14:47:08 +0200

    add component for displying the AI result

---
+++ client/src/components/TaskPlanningDisplay.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+const TaskPlanningDisplay = ( taskPlanning ) => {
+  
+  return (
+    <>
+    </>
+  )
+}
+
+export default TaskPlanningDisplay
--- server/AIIntegration.js
+++ server/AIIntegration.js
@@ -3,14 +3,14 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 
 dotenv.config();
 
-export const generateQuiz = async (projectOrTaskDescription, type, assignee, projectName) => {
+export const generateQuiz = async (taskDescription, type, assignee, projectName) => {
   const genAI = new GoogleGenerativeAI(process.env.API_KEY);
   const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
   const prompt = `
   The user wants to plan a project/task. The goal is to return a hierarchical breakdown of tasks and subtasks that would be suitable for a project management system like JIRA. The AI will respond in JSON format.
 
-  The user will input a description of the task or project ${projectOrTaskDescription}, the type ${type}, the assignee ${assignee}, and the project name ${projectName}. Based on the ${type}, which indicates the level of granularity (e.g., Project, Epic, Story, Task), the AI should break it down into Epics, Stories, Tasks, and Sub-tasks with detailed descriptions for each task.
+  The user will input a description of the task or project ${taskDescription}, the type ${type}, the assignee ${assignee}, and the project name ${projectName}. Based on the ${type}, which indicates the level of granularity (e.g., Project, Epic, Story, Task), the AI should break it down into Epics, Stories, Tasks, and Sub-tasks with detailed descriptions for each task.
 
   If the ${type} is 'Project', return the full breakdown of tasks and subtasks for the project (Epics, Stories, Tasks, Sub-tasks). If the ${type} is any other value (Epic, Story, Task), return the breakdown for that specific level.
 
@@ -30,7 +30,7 @@ export const generateQuiz = async (projectOrTaskDescription, type, assignee, pro
   How would you break down the following task/project? Please respond with a nested JSON output of all the epics, stories, tasks, and sub-tasks formatted for JIRA:
 
   Project Name: ${projectName}
-  Project/Task Description: ${projectOrTaskDescription}
+  Project/Task Description: ${taskDescription}
   Type: ${type}
   Assignee: ${assignee}
 `;
